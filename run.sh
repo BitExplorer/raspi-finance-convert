82d314bd0a9fef21fe9da259580e5ee63732c52c
Brian Henning <henninb@msn.com>
2019-11-08 12:21:25 -0600

    updates

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#RASPI_IP=$(nmap -sP --host-timeout 10 192.168.100.0/24 | grep raspb | grep -o '[0-9.]\+[0-9]')
 mkdir -p logs ssl json_in
 HOST_BASEDIR=$(pwd)
 GUEST_BASEDIR=/opt/raspi_finance_convert
@@ -10,7 +11,8 @@ touch env
 echo ./mvnw package -Dmaven.test.skip=true
 echo gradle wrapper --gradle-version 5.6.2 --distribution-type all
 ./gradlew clean build
+rm -rf LOGS_IS_UNDEFINED
 docker build -t raspi_finance_convert .
-docker run -it -h raspi_finance_convert --add-host hornsup:$HOST_IP -p 8080:8080 --env-file env.secrets --env-file env -rm -v $HOST_BASEDIR/logs:$GUEST_BASEDIR/logs -v $HOST_BASEDIR/ssl:$GUEST_BASEDIR/ssl -v $HOST_BASEDIR/json_in:$GUEST_BASEDIR/json_in --rm --name raspi_finance_convert raspi_finance_convert
+docker run -it -h raspi_finance_convert --add-host hornsup:$HOST_IP -p 8081:8080 --env-file env.secrets --env-file env -v $HOST_BASEDIR/logs:$GUEST_BASEDIR/logs -v $HOST_BASEDIR/ssl:$GUEST_BASEDIR/ssl -v $HOST_BASEDIR/json_in:$GUEST_BASEDIR/json_in --rm --name raspi_finance_convert raspi_finance_convert
 
 exit 0
