b666f4dade185443e076033a123d2e0f7361b5a2
Brian Henning <henninb@msn.com>
2019-11-08 12:01:17 -0600

    updates

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,16 @@
 FROM openjdk:8
 
 RUN useradd henninb
-RUN mkdir -p /opt/raspi_finance/bin /opt/raspi_finance/ssl /opt/raspi_finance/logs /opt/raspi_finance/json_in
-RUN chown -R henninb /opt/raspi_finance/*
+RUN mkdir -p /opt/raspi_finance_convert/bin /opt/raspi_finance_convert/ssl /opt/raspi_finance_convert/logs /opt/raspi_finance_convert/json_in
+RUN chown -R henninb /opt/raspi_finance_convert/*
 
-COPY ./build/libs/raspi_finance_convert.jar /opt/raspi_finance/bin/raspi_finance.jar
-WORKDIR /opt/raspi_finance/bin
-#CMD ["java", "-jar" "/opt/raspi_finance/raspi_finance.jar"]
+COPY ./build/libs/raspi_finance_convert.jar /opt/raspi_finance_convert/bin/raspi_finance_convert.jar
+WORKDIR /opt/raspi_finance_convert/bin
+#CMD ["java", "-jar" "/opt/raspi_finance_convert/raspi_finance_convert.jar"]
 
 #RUN echo "172.17.0.1 hornsup" | tee -a /etc/hosts
 #RUN ping $(ip route|awk '/default/ { print $3 }')
 #hornsup:9092
 USER henninb
 
-CMD java -jar raspi_finance.jar
+CMD java -jar raspi_finance_convert.jar
--- run.sh
+++ run.sh
@@ -2,14 +2,15 @@
 
 mkdir -p logs ssl json_in
 HOST_BASEDIR=$(pwd)
-GUEST_BASEDIR=/opt/raspi_finance
-HOST_IP=$(ipconfig getifaddr en0)
+GUEST_BASEDIR=/opt/raspi_finance_convert
+#HOST_IP=$(ifconfig getifaddr en0)
+HOST_IP=$(hostname -I | awk '{print $1}')
 export LOGS=$BASEDIR/logs
 touch env
 echo ./mvnw package -Dmaven.test.skip=true
 echo gradle wrapper --gradle-version 5.6.2 --distribution-type all
 ./gradlew clean build
-docker build -t raspi_finance .
-docker run -it -h raspi_finance --add-host hornsup:$HOST_IP -p 8080:8080 --env-file env -v $HOST_BASEDIR/logs:$GUEST_BASEDIR/logs -v $HOST_BASEDIR/ssl:$GUEST_BASEDIR/ssl -v $HOST_BASEDIR/json_in:$GUEST_BASEDIR/json_in --rm --name raspi_finance raspi_finance
+docker build -t raspi_finance_convert .
+docker run -it -h raspi_finance_convert --add-host hornsup:$HOST_IP -p 8080:8080 --env-file env.secrets --env-file env -rm -v $HOST_BASEDIR/logs:$GUEST_BASEDIR/logs -v $HOST_BASEDIR/ssl:$GUEST_BASEDIR/ssl -v $HOST_BASEDIR/json_in:$GUEST_BASEDIR/json_in --rm --name raspi_finance_convert raspi_finance_convert
 
 exit 0
