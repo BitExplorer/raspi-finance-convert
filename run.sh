bb92d2bbce7d61e2a6f61f9e219a330233a57772
Brian Henning <henninb@msn.com>
2020-01-30 17:36:53 -0600

    docker update; bootrun; and logging updates

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home
+export PATH=${JAVA_HOME}/bin:${PATH}
+
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <prod or local>"
   exit 1
